25bc48a2836e8a22bed8c0f17b37e2517fadff12
chen <2463238851@qq.com>
2022-02-01 22:44:25 +0800

    了解一些基本得使用

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import axios from "axios";
+import InterceptorManager from "axios/lib/core/InterceptorManager";
+
+// // 实际上我们导入得是一个实例
+// axios.get("localhost:8080", {
+//     data: {}
+// });
+
+// axios.interceptors.request.use(
+//     config => config,
+//     error => Promise.reject(error),
+//     {}
+// );
+
+// axios.post("localhost:8080", {}, {});
+const request = new InterceptorManager();
+
+// 返回一个id => 在数组中得索引
+const id = request.use(() => {
+    console.log("fn1");
+});
+request.use(() => {
+    console.log("fn2");
+});
+// 取消使用当前拦截器
+request.eject(id);
